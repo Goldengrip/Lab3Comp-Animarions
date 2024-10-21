fcbccdb05077cf26b6a497cc5fb462823c87cbc4
Goldengrip <Sergiodesouza@hotmail.ca>
2024-10-21 15:05:51 -0400

    Animations

--- Lab3/Assets/Script/MeshController.cs
+++ Lab3/Assets/Script/MeshController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -8,15 +9,43 @@ public class MeshController : MonoBehaviour
     public GameObject Target;
     private NavMeshAgent agent;
 
+    bool isWalking=true;
+    private Animator animator;
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        agent.destination = Target.transform.position;
+        if(isWalking)
+            agent.destination = Target.transform.position;
+        else
+        {
+           agent.destination = transform.position; 
+        }
+        
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Dragon")
+        {
+            isWalking = false;
+            animator.SetTrigger("Attack");
+        }
+    }
+    private void OnTriggerExit(Collider other) 
+    {
+        if(other.name == "Dragon")
+        {
+            isWalking = true;
+            animator.SetTrigger("Walking");
+        }
+    }
+
 }
